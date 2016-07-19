f14ab2eee7a844266bdd963d590d2e2b2aac7796
minhthanh2345 <nguyentronga7@gmail.com>
2016-07-19 19:26:20 +0700

    Update bannerdautrang.js

--- bannerdautrang.js
+++ bannerdautrang.js
@@ -117,7 +117,7 @@ $(window).load(function() {
 			$(divpopup).attr('id', 'myModal');
 			$(divpopup).attr('class', 'linhnguyen-modal');
 			$( "body" ).append(divpopup);
-			$("#myModal").html("<a http://media.bizwebmedia.net/sites/139302/upload/Logo/201672185939117.jpg?v=175' width='500px'/>Túi xách, Giày dép caigicungre.com.vn. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
+			$("#myModal").html("<a http://media.bizwebmedia.net/sites/139302/upload/Logo/201672185939117.jpg?v=175' width='500px'/>SỨC KHỎE VÀ SỨC ĐẸP youcare.vn. <h2><a class='close-linhnguyen-modal'>X</a></h2>");
 			$('#myModal').linhnguyen($('#myModal').data());
 		}
 });
